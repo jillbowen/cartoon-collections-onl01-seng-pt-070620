46c7c88d8e654f611a33fdf3436f17bbb852bf54
Jill Short <jillian.b.short@gmail.com>
2020-07-16 17:39:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ end
 
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.include? ()
+  if cheddar_cheese.include? ("cheddar", "gouda", "camembert")
 end
